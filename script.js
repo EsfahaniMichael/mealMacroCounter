eff28e52d65cb0dc8b68d4956c7f1a27bc5f247e
Esfahani <esfahanimichael@gmail.com>
2018-10-21 14:46:15 -0700

    Updates to SGT 2.0

--- script.js
+++ script.js
@@ -1,91 +1,41 @@
-/* information about jsdocs: 
-* param: http://usejsdoc.org/tags-param.html#examples
-* returns: http://usejsdoc.org/tags-returns.html
-* 
-/**
- * Listen for the document to load and initialize the application
- */
+
 $(document).ready(initializeApp);
 
-/**
- * Define all global variables here.  
- */
 var student_array = [];
 var gradeAverage = null;
-/***********************
- * student_array - global array to hold student objects
- * @type {Array}
- * example of student_array after input: 
- * student_array = [
- *  { name: 'Jake', course: 'Math', grade: 85 },
- *  { name: 'Jill', course: 'Comp Sci', grade: 85 }
- * ];
- */
 
-/***************************************************************************************************
-* initializeApp 
-* @params {undefined} none
-* @returns: {undefined} none
-* initializes the application, including adding click handlers and pulling in any data from the server, in later versions
-*/
 function initializeApp(){
     addClickHandlersToElements();
 }
 
-/***************************************************************************************************
-* addClickHandlerstoElements
-* @params {undefined} 
-* @returns  {undefined}
-*     
-*/
 function addClickHandlersToElements(){
     $('#button1').on('click', handleAddClicked);
     $('#button2').on('click', handleCancelClick);
     $('#button3').on('click', handleServerClick);
 }
 
-/***************************************************************************************************
- * handleAddClicked - Event Handler when user clicks the add button
- * @param {object} event  The event object from the click
- * @return: 
-       none
- */
 function handleAddClicked(){
-
     addStudent();
 }
 
-
-/***************************************************************************************************
- * handleCancelClicked - Event Handler when user clicks the cancel button, should clear out student form
- * @param: {undefined} none
- * @returns: {undefined} none
- * @calls: clearAddStudentFormInputs
- */
 function handleCancelClick(){
     clearAddStudentFormInputs();
 }
-/***************************************************************************************************
- * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
- * @param {undefined} none
- * @return undefined
- * @calls clearAddStudentFormInputs, updateStudentList
- */
+
 function addStudent(){
     var studentName = $('#studentName').val();
     var courseName = $('#course').val();
     var studentGrade = $('#studentGrade').val();
      var student = {
-        name: studentName, course: courseName, grade: studentGrade
+         api_key: "nwlHxOXLdQ", name: studentName, course: courseName, grade: studentGrade
     }
 
     student_array.push(student);
+     addingManual(student);
     updateStudentList(student_array);
     clearAddStudentFormInputs();
 }
-/***************************************************************************************************
- * clearAddStudentForm - clears out the form values based on inputIds variable
- */
+
 function clearAddStudentFormInputs(){
 
     $('#course').val('');
@@ -93,19 +43,16 @@ function clearAddStudentFormInputs(){
     $('#studentGrade').val('');
 }
 
-/***************************************************************************************************
- * renderStudentOnDom - take in a student object, create html elements from the values and then append the elements
- * into the .student_list tbody
- * @param {object} studentObj a single student object with course, name, and grade inside
- */
 function renderStudentOnDom(studentObj){
-
+    var manualDeletedData = {};
    var name = $('<td>').text(studentObj.name);
     var course = $('<td>').text(studentObj.course);
     var grade  = $('<td>').text(studentObj.grade);
     var index = student_array.indexOf(studentObj);
     var deleteButton = $('<td>').html("<button type ='button' class= 'btn btn-danger btn-xs'> Delete </button> ").click(function(){
-
+        manualDeletedData.api_key = "nwlHxOXLdQ";
+        manualDeletedData.student_id = studentObj.id;
+        deletingManual(manualDeletedData);
         tr.remove();
         student_array.splice(index, 1);
         updateStudentList(student_array);
@@ -116,27 +63,16 @@ function renderStudentOnDom(studentObj){
     $('tbody').append(tr);
 }
 
-/***************************************************************************************************
- * updateStudentList - centralized function to update the average and call student list update
- * @param students {array} the array of student objects
- * @returns {undefined} none
- * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
- */
 function updateStudentList(array){
     $('tbody').empty();
     for (var studentListIndex = 0; studentListIndex < array.length; studentListIndex++){
         renderStudentOnDom(array[studentListIndex]);
-        // student_array.push(array[studentListIndex]);
     }
 
   var gradeAverage = calculateGradeAverage(array);
   renderGradeAverage(gradeAverage);
 }
-/***************************************************************************************************
- * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
- * @param: {array} students  the array of student objects
- * @returns {number}
- */
+
 function calculateGradeAverage(bigArray){
     var average = null;
     for (calculateIndex = 0; calculateIndex < bigArray.length; calculateIndex++){
@@ -144,23 +80,14 @@ function calculateGradeAverage(bigArray){
     }
     average = average / (bigArray.length);
     return average;
-
 }
-/***************************************************************************************************
- * renderGradeAverage - updates the on-page grade average
- * @param: {number} average    the grade average
- * @returns {undefined} none
- */
+
 function renderGradeAverage(average){
     $('.avgGrade').text(average);
-
 }
-
-
 
 function handleServerClick(){
     var myData = {api_key: "nwlHxOXLdQ"}
-
     var ajaxConfig = {
         dataType: 'json',
         url: "http://s-apis.learningfuze.com/sgt/get",
@@ -173,28 +100,32 @@ function handleServerClick(){
         },
     }
     $.ajax(ajaxConfig);
-
 }
 
-function addingManual(){
-    var myData = {api_key: "nwlHxOXLdQ"}
-
+function addingManual(student){
     var ajaxConfig = {
         dataType: 'json',
         url: "http://s-apis.learningfuze.com/sgt/create",
         method: "Post",
-        data: myData,
+        data: student,
         success: function(response){
-            student_array.push(response.data);
-            updateStudentList(student_array);
-            console.log("get data function worked " + response);
-        },
+            console.log('hi',response);
+        }
     }
     $.ajax(ajaxConfig);
-
-
 }
 
-
+function deletingManual(student){
+    var ajaxConfig = {
+        dataType: 'json',
+        url: "http://s-apis.learningfuze.com/sgt/delete",
+        method: "Post",
+        data: student,
+        success: function(response){
+            console.log('hi',response);
+        }
+    }
+    $.ajax(ajaxConfig);
+}
 
 
